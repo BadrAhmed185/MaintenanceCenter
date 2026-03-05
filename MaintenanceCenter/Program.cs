f22229edb3a089a39a0147dec32df55e3d60d2de
Badr Elbadry <badr.ahmed.2015@gmail.com>
2026-03-05 11:03:42 +0300

    disble https temporarely

--- MaintenanceCenter/Program.cs
+++ MaintenanceCenter/Program.cs
@@ -187,12 +187,12 @@ namespace MaintenanceCenter
             {
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                   app.UseHsts();
+                //   app.UseHsts();
 
 
             }
 
-            app.UseHttpsRedirection();
+      //      app.UseHttpsRedirection();
             // ... down in the pipeline ...
             app.UseStaticFiles(); // Required to serve CSS, JS, Images from wwwroot
             app.UseRouting();
